b411ec3e8fb6dcf9240213446dd80f19fe00660f
meongju0o0 <bl4angel20@gmail.com>
2023-09-24 16:15:11 +0900

    cache clear, add ipconfig

--- partition_graph.py
+++ partition_graph.py
@@ -6,7 +6,6 @@ import torch as th
 from dgl.data import RedditDataset
 from ogb.nodeproppred import DglNodePropPredDataset
 
-
 def load_reddit(self_loop=True):
     """Load reddit dataset."""
     data = RedditDataset(self_loop=self_loop)
