8bca945a0ac3fd9754017e4c01707c3a8910a2c8
meongju0o0 <bl4angel20@gmail.com>
2023-09-27 15:30:11 +0900

    update loss function

--- loss.py
+++ loss.py
@@ -6,23 +6,26 @@ class HLoss(nn.Module):
     """
     Loss for Regularizing
     Penalizes unconfident predictions and sharpens predictions
-
-    Parameters
-    ----------
-    x : DistTensor
-        Predicted Value
-
-    Returns
-    -------
-    Loss
     """
 
     def __init__(self):
         super(HLoss, self).__init__()
 
-    def forward(self, x, full=False):
-        num_data = x.shape[0]
-        b = F.softmax(x, dim=1) * F.log_softmax(x, dim=1)
+    def forward(self, pred, full=False):
+        """
+        Parameters
+        ----------
+        pred : DistTensor
+            Predicted Value
+        full : bool
+            Decide whether to calculate sum of the loss or mean of the loss
+
+        Returns
+        -------
+        Calculated Loss
+        """
+        num_data = pred.shape[0]
+        b = F.softmax(pred, dim=1) * F.log_softmax(pred, dim=1)
         if full:
             return -1.0 * b.sum(1)
         b = -1.0 * b.sum()
@@ -34,24 +37,25 @@ class XeLoss(nn.Module):
     """
     Loss using Kullback-Leibler Divergence
     Encourages the consistency of predictions on two consecutive augmented samples G^(t), G^(t+1)
-
-    Parameters
-    ----------
-    y : DistTensor
-        Label
-    x : DistTensor
-        Predicted Value
-
-    Returns
-    -------
-    Loss
     """
     def __init__(self):
         super(XeLoss, self).__init__()
 
-    def forward(self, y, x):
-        num_data = x.shape[0]
-        b = F.softmax(y, dim=1) * F.log_softmax(x, dim=1) - F.softmax(y, dim=1) * F.log_softmax(y, dim=1)
+    def forward(self, pred, label):
+        """
+        Parameters
+        ----------
+        pred : DistTensor
+            Predicted Value
+        label : DistTensor
+            Label
+
+        Returns
+        -------
+        Calculated Loss
+        """
+        num_data = pred.shape[0]
+        b = F.softmax(label, dim=1) * F.log_softmax(pred, dim=1) - F.softmax(label, dim=1) * F.log_softmax(label, dim=1)
         b = -1.0 * b.sum()
         b = b / num_data
         return b
@@ -60,25 +64,26 @@ class XeLoss(nn.Module):
 class Jensen_Shannon(nn.Module):
     """
     Symmetrical KL Divergence
-
-    Parameters
-    ----------
-    y : DistTensor
-        Label
-    x : DistTensor
-        Predicted Value
-
-    Returns
-    -------
-    Loss
     """
     def __init__(self):
         super(Jensen_Shannon, self).__init__()
 
-    def forward(self, y, x):
-        num_data = x.shape[0]
-        b = F.softmax(y, dim=1) * F.log_softmax(x, dim=1) - F.softmax(y, dim=1) * F.log_softmax(y, dim=1)
-        b += F.softmax(x, dim=1) * F.log_softmax(y, dim=1) - F.softmax(x, dim=1) * F.log_softmax(x, dim=1)
+    def forward(self, pred, label):
+        """
+        Parameters
+        ----------
+        pred : DistTensor
+            Predicted Value
+        label : DistTensor
+            Label
+
+        Returns
+        -------
+        Calculated Loss
+        """
+        num_data = pred.shape[0]
+        b = F.softmax(label, dim=1) * F.log_softmax(pred, dim=1) - F.softmax(label, dim=1) * F.log_softmax(label, dim=1)
+        b += F.softmax(pred, dim=1) * F.log_softmax(label, dim=1) - F.softmax(pred, dim=1) * F.log_softmax(pred, dim=1)
         b = -0.5 * b.sum()
         b = b / num_data
         return b
