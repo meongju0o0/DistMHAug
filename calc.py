fe5224cbb3f6a8b0c7023f17c45f4a378ea5f990
meongju0o0 <bl4angel20@gmail.com>
2023-09-28 02:00:38 +0900

    add calc.py

---
+++ calc.py
@@ -0,0 +1,21 @@
+import torch as th
+import dgl
+from scipy.stats import truncnorm
+
+
+def log_normal(a, b, sigma):
+    return -1 * th.pow(a - b, 2) / (2 * th.pow(sigma, 2))
+
+
+def our_truncnorm(a, b, mu, sigma, x=None, mode='pdf'):
+    a, b = (a - mu) / sigma, (b - mu) / sigma
+    if mode=='pdf':
+        return truncnorm.pdf(x, a, b, loc = mu, scale = sigma)
+    elif mode=='rvs':
+        return truncnorm.rvs(a, b, loc = mu, scale = sigma)
+
+
+def aggregate(g, agg_model):
+    g = dgl.add_self_loop(g)
+    s_vec = agg_model(g)
+    return s_vec
