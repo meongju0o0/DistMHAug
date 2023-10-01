06a3d74cad778583f35a764c45d299b2b304c277
meongju0o0 <bl4angel20@gmail.com>
2023-10-01 22:44:11 +0900

    Update augmentation.py

--- augmentation/augmentation.py
+++ augmentation/augmentation.py
@@ -1,9 +1,13 @@
 import dgl
 import torch as th
+import numpy as np
 from scipy.stats import truncnorm
+from scipy.special import betaln
 
-from calc import log_normal
 from drop import MHEdgeDropping, MHNodeDropping
+from ..training.loss import HLoss
+from ..training.model import SimpleAGG
+from ..common.calc import log_normal
 
 
 def aggregate(g, agg_model):
@@ -16,7 +20,7 @@ def augment(org_g, delta_g_e_aug, delta_g_v_aug):
     aug_g = MHNodeDropping(aug_g, delta_g_v_aug)
     return aug_g
 
-def mh_algorithm(args, org_g, prev_aug_g, model, loss_fcn, dataloader, device):
+def mh_algorithm(args, org_g, prev_aug_g, model, dataloader, device):
     delta_g_e = 1 - prev_aug_g.num_edges() / org_g.num_edges()
     delta_g_e_aug = truncnorm.rvs(0, 1, loc=delta_g_e, sigma=args.sigma_delta_e)
 
@@ -25,16 +29,20 @@ def mh_algorithm(args, org_g, prev_aug_g, model, loss_fcn, dataloader, device):
 
     cur_aug_g = augment(org_g, delta_g_e_aug, delta_g_v_aug)
 
+    agg_model = SimpleAGG
+    h_loss = HLoss()
+
     model.eval()
 
     num_seeds = 0
     num_inputs = 0
-    step_cnt = 0
-    loss_sum = 0
+    batch_cnt = 0
+    ent_sum = 0
 
     with th.no_grad():
         for step, (input_nodes, seeds, blocks) in enumerate(dataloader):
-            step_cnt += 1
+            batch_cnt += 1
+
             # Slice feature and label.
             batch_inputs = org_g.ndata["features"][input_nodes]
             batch_labels = org_g.ndata["labels"][seeds].long()
@@ -46,9 +54,31 @@ def mh_algorithm(args, org_g, prev_aug_g, model, loss_fcn, dataloader, device):
             batch_inputs = batch_inputs.to(device)
             batch_labels = batch_labels.to(device)
 
-            # Compute loss and prediction.
+            # Get prediction to calculate ent.
             batch_pred = model(blocks, batch_inputs)
-            loss = loss_fcn(batch_pred, batch_labels)
-            loss_sum += loss
 
-        loss_mean = loss_sum / step_cnt
+            max_ent = h_loss(th.full((1, batch_pred.shape[1]), 1 / batch_pred.shape[1])).item()
+            ent = h_loss(batch_pred.detach(), True) / max_ent
+            ent_sum += ent
+
+        ent_mean = ent_sum / batch_cnt
+        org_ego = aggregate(org_g, agg_model)
+
+        delta_g_e_ = 1 - (aggregate(cur_aug_g, agg_model) / org_ego).squeeze(1)
+        delta_g_aug_e_ = 1 - (aggregate(cur_aug_g, agg_model) / org_ego).squeeze(1)
+        delta_g_v_ = 1 - (aggregate(cur_aug_g, agg_model) / org_ego).squeeze(1)
+        delta_g_aug_v_ = 1 - (aggregate(cur_aug_g, agg_model) / org_ego).squeeze(1)
+
+    p = args.lam1_e * log_normal(delta_g_e, args.mu_e, args.a_e * ent_mean + args.b_e) + \
+        args.lam1_v * log_normal(delta_g_v, args.mu_v, args.a_v * ent_mean + args.b_v)
+    p_aug = args.lam1_e * log_normal(delta_g_aug_e_, args.mu_e, args.a_e * ent_mean + args.b_e) + \
+            args.lam1_v * log_normal(delta_g_aug_v_, args.mu_v, args.a_v * ent_mean + args.b_v)
+
+    q = np.log(truncnorm.pdf(delta_g_e, 0, 1, loc=delta_g_e_aug, scale=args.sigma_delta_e)) + \
+        args.lam2_e * betaln(org_g.num_edges() - org_g.num_edges() * delta_g_e + 1, org_g.num_edges() * delta_g_e + 1) + \
+        np.log(truncnorm.pdf(delta_g_v, 0, 1, loc=delta_g_v_aug, scale=args.sigma_delta_v)) + \
+        args.lam2_v * betaln(org_g.num_nodes() - org_g.num_nodes() * delta_g_v + 1, org_g.num_nodes() * delta_g_v + 1)
+    q_aug = np.log(truncnorm.pdf(delta_g_e_aug, 0, 1, loc=delta_g_e, scale=args.sigma_delta_e)) + \
+            args.lam2_e * betaln(org_g.num_edges() - org_g.num_edges() * delta_g_e_aug + 1, org_g.num_edges() * delta_g_e_aug + 1) + \
+            np.log(truncnorm.pdf(delta_g_v_aug, 0, 1, loc=delta_g_v, scale=args.sigma_delta_v)) + \
+            args.lam2_v * betaln(org_g.num_nodes() - org_g.num_nodes() * delta_g_v_aug + 1, org_g.num_nodes() * delta_g_v_aug + 1)
