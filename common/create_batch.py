f95321994db965e70da74e9a0263615eceda19cb
meongju0o0 <bl4angel20@gmail.com>
2023-10-05 22:24:31 +0900

    add create_batch.py

---
+++ common/create_batch.py
@@ -0,0 +1,45 @@
+import dgl
+
+
+class AugDataLoader:
+    def __init__(self, org_g, prev_g, cur_g, train_nid, sampler, batch_size, shuffle=True, drop_last=False):
+        self.org_g = org_g
+        self.prev_g = prev_g
+        self.cur_g = cur_g
+
+        self.dataloader = dgl.dataloading.DistNodeDataLoader(
+            org_g, train_nid, sampler, batch_size=batch_size, shuffle=shuffle, drop_last=drop_last)
+
+        self.src_nodes_list = []
+        self.dst_nodes_list = []
+        self.blocks = {"org_blocks": [], "prev_blocks": [], "cur_blocks": []}
+
+        self.limit = 0
+        self.idx = 0
+
+    def __iter__(self):
+        self.limit = 0
+        self.idx = 0
+
+        for src_nodes, dst_nodes, blocks in self.dataloader:
+            self.limit += 1
+
+            self.src_nodes_list.append(src_nodes)
+            self.dst_nodes_list.append(dst_nodes)
+
+            self.blocks["org_blocks"].append(blocks)
+            self.blocks["prev_blocks"].append(
+                dgl.to_block(self.prev_g, dst_nodes=dst_nodes, src_nodes=src_nodes, include_dst_in_src=True))
+            self.blocks["cur_blocks"].append(
+                dgl.to_block(self.cur_g, dst_nodes=dst_nodes, src_nodes=src_nodes, include_dst_in_src=True))
+
+        return self
+
+    def __next__(self):
+        if self.idx < self.limit:
+            return (self.src_nodes_list[self.idx], self.src_nodes_list[self.idx],
+                    self.blocks["org_blocks"][self.idx],
+                    self.blocks["prev_blocks"][self.idx],
+                    self.blocks["cur_blocks"][self.idx])
+        else:
+            return StopIteration
