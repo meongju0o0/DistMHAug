40a125cea3cedd17f7523e702fd59a95250602c1
meongju0o0 <bl4angel20@gmail.com>
2023-10-08 23:12:46 +0900

    add config

--- common/config.py
+++ common/config.py
@@ -1,3 +1,7 @@
 CONFIG = {
-
+    'a_e': 100, 'b_e': 1, 'a_v': 100, 'b_v': 1,
+    'kl': 2.0, 'h': 0.2,
+    'sigma_delta_e': 0.03, 'sigma_delta_v': 0.03,
+    'mu_e': 0.6, 'mu_v': 0.2,
+    'lam1_e': 1, 'lam1_v': 1, 'lam2_e': 0.000, 'lam2_v': 0.000
 }
