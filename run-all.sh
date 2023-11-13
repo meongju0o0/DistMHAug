8ac81f09759bc64099c762584b8d5baa04b31636
meongju0o0 <bl4angel20@gmail.com>
2023-11-13 15:57:35 +0900

    update gitignore

--- run-all.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/env bash
-
-chmod +x run-citeseer.sh run-cora.sh run-ogbn-papers100M.sh run-ogbn-products.sh
-
-./run-citeseer.sh && \
-./run-cora.sh && \
-./run-ogbn-papers100M.sh && \
-./run-ogbn-products.sh
