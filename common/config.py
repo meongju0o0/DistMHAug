88cbf19924614507400ec3838386620355824bf4
meongju0o0 <bl4angel20@gmail.com>
2023-12-12 03:48:17 +0900

    update config.py

--- common/config.py
+++ common/config.py
@@ -14,11 +14,11 @@ CONFIG = {
         'lam1_e': 1, 'lam1_v': 0.9999, 'lam2_e': 10, 'lam2_v': 0.999
     },
     "ogb-product": {
-        'a_e': 100, 'b_e': 1, 'a_v': 100, 'b_v': 1,
-        'kl': 2.0, 'h': 0.2,
-        'sigma_delta_e': 0.03, 'sigma_delta_v': 0.03,
-        'mu_e': 0.6, 'mu_v': 0.2,
-        'lam1_e': 1, 'lam1_v': 1, 'lam2_e': 0.000, 'lam2_v': 0.000
+        'a_e': 5, 'b_e': 1, 'a_v': 1, 'b_v': 1,
+        'kl': 0.5, 'h': 0.5,
+        'sigma_delta_e': 0.05, 'sigma_delta_v': 0.05,
+        'mu_e': 0, 'mu_v': 0,
+        'lam1_e': 1, 'lam1_v': 1, 'lam2_e': 1, 'lam2_v': 1
     },
 }
 
